b90069efd25c324013e60e5810066f20eb35ef75
Suresh203-java <sureshsingarapu30@gmail.com>
2026-05-30 10:21:15 +0530

    Add files via upload

---
+++ iterdemo.java
@@ -0,0 +1,11 @@
+public class iterdemo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Simple Iteration using FOR Loop : ");
+        for(int i = 0; i <= 5; i++)
+        {
+            System.out.println("After Iteration : " +i);
+        }
+    }
+}
