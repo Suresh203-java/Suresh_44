59a02a1e9e0e3f8cada46e5cd8962e625c3bac0f
Suresh203-java <sureshsingarapu30@gmail.com>
2026-05-30 10:29:45 +0530

    Add files via upload

---
+++ forlist.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class forlist
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Lion");
+        animals.add("Fox");
+        animals.add("Tiger");
+        for(String animal : animals)
+        {
+            System.out.println("Wild animal in the Forest : "+animal);
+        }
+    }
+}
