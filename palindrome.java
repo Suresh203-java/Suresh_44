bd480907c30723481595f7abe98755d94e78c0ad
Suresh203-java <sureshsingarapu30@gmail.com>
2026-06-03 14:19:27 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,23 @@
+public class palindrome
+{
+    public static void main(String[] args)
+    {
+        int num = 121;
+        int temp = num;
+        int reverse = 0;
+        while (temp != 0)
+        {
+            int digit = temp % 10;
+            reverse = reverse * 10 + digit;
+            temp = temp / 10;
+        }
+        if(num == reverse)
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
---
+++ sum_num.java
@@ -0,0 +1,14 @@
+public class sum_num
+{
+    public static void main(String[] args)
+    {
+        int num = 1239385;
+        int sum = 0;
+        while(num != 0)
+        {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
